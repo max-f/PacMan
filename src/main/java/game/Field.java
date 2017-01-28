7482ac29cfb6c345f6365b85f6ec5f0794cd7f77
Max <maaax.fischer@gmail.com>
2017-01-28 11:24:30 +0100

    Missing field functionality and some docstrings added

--- src/main/java/game/Field.java
+++ src/main/java/game/Field.java
@@ -58,22 +58,62 @@ public class Field {
         return null;
     }
 
+    /**
+     * Placing a pac-dot on the {@code Field}. It is only allowed to do so on a
+     * field of type FREE.
+     *
+     * @param pacDot if a pac-dot is to be set or not.
+     * @throws IllegalStateException if the {@code Field} is not of type FREE.
+     */
     public void setPacDot(boolean pacDot) {
         if (type != FieldType.FREE) {
-            throw new IllegalStateException("illegal fieldType");
+            throw new IllegalStateException("invalid field type to place a pac-dot");
         }
         this.pacDot = pacDot;
     }
 
+    /**
+     * Placing a power pellet on the {@code Field}. It is only allowed to do so on a
+     * field of type POWER_PELLET.
+     *
+     * @param powerPellet if a power pellet is to be set or not.
+     * @throws IllegalStateException if the {@code Field} is not of type POWER_PELLET.
+     */
     public void setPowerPellet(boolean powerPellet) {
         if (type != FieldType.POWER_PELLET) {
-            throw new IllegalStateException("illegal fieldtype");
+            throw new IllegalStateException("invalid field type to place a power pellet");
         }
+        this.powerPellet = powerPellet;
     }
 
+    /**
+     * Places an {@code ExtraItem} on the field. It is only allowed to do so on a field
+     * which is not of type WALL or GHOST_START.
+     *
+     * @param extraItem the {@code ExtraItem} to place on the field.
+     * @throws IllegalStateException if the {@code Field} is of type WALL or GHOST_START.
+     */
+    public void setExtraItem(ExtraItem extraItem) {
+        if (type == FieldType.WALL || type == FieldType.GHOST_START) {
+            throw new IllegalStateException("invalid field type to place an extra item");
+        }
+        this.extraItem = extraItem;
+    }
+
+    /**
+     * Placing the {@code Actor} on the {@code Field}.
+     *
+     * @param actor The {@code Actor} to enter the field
+     * @throws IllegalArgumentException if the given {@code Actor} is null.
+     * @throws IllegalStateException if the {@code Actor} can not be placed on the {@code Field}.
+     * 
+     */
     public void placeActor(Actor actor) {
         if (actor == null) {
             throw new IllegalArgumentException("bad actor argument");
+        }
+        if (this.checkActor(actor) == Event.MOVE_IMPOSSIBLE) {
+            throw new IllegalStateException("actor can not be placed here");
         }
         this.actor = actor;
         if (actor.getActorType() == ActorType.PACMAN) {
@@ -87,6 +127,14 @@ public class Field {
         this.actor = null;
     }
 
+    /**
+     * This method checks what happens if the given actor enters this field and returns
+     * the corresponding event.
+     *
+     * @param incomingActor the ${@code Actor} that wants to enter the field.
+     * @return the ${@code Event} that occurs if this actor would enter the field.
+     * @throws IllegalArgumentException if null is passed as {@code incomingActor}
+     */
     public Event checkActor(Actor incomingActor) {
         if (incomingActor == null) {
             throw new IllegalArgumentException("bad incoming actor argument");
@@ -128,6 +176,14 @@ public class Field {
         return e;
     }
 
+    /**
+     * The String of a ${@code Field} should have the following format:
+     * [Fieldtype, PacDot?, PowerPellet?, ExtraItem, ActorType]
+     * The last two entries are optional. If they are null, they don't appear
+     * in the result string.
+     *
+     * @return  the string representation
+     */
     public String toString() {
         StringBuilder sb = new StringBuilder("[");
         List<String> fields = new ArrayList<String>();
