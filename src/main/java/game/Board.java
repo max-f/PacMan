2d349524a28ea970644ca1d721e0997d1e2db9d1
Max <maaax.fischer@gmail.com>
2017-01-30 22:29:49 +0100

    Implemented board constructor and board init method

--- src/main/java/game/Board.java
+++ src/main/java/game/Board.java
@@ -15,10 +15,47 @@ public class Board {
     private List<Point2D> ghostsStarts = new ArrayList<>();
     private Point2D pacManStart;
 
+    /**
+     * Creates a new board with given rows and columns from the {@code Field} array. Saves the start position of
+     * the ghosts, checks the amount of free fields and Pac-Man starting fields, and populates the list of
+     * {@code extraItemFields}.
+     *
+     * @param board the board that specifies the map.
+     * @throws IllegalArgumentException if the given board is null, has no free fields, not exactly one Pac-Man
+     * start, a dimension is zero, the rows have different length, or the given board contains a null.
+     */
     public Board(Field[][] board) {
         if (board == null || board.length == 0 || board[0].length == 0) {
-
+            throw new IllegalArgumentException("invalid board");
         }
+        int pacmanStartCounter = 0;
+        int freeFieldsCounter = 0;
+
+        for (int i = 0; i< board.length; i++) {
+            for (int j = 0; j < board[i].length; j++) {
+                switch (board[i][j].getType()) {
+                    case PACMAN_START:
+                        pacmanStartCounter++;
+                        extraItemFields.add(new Point2D(i, j));
+                        break;
+                    case FREE:
+                        freeFieldsCounter++;
+                        extraItemFields.add(new Point2D(i, j));
+                        break;
+                    case POWER_PELLET:
+                        extraItemFields.add(new Point2D(i, j));
+                        break;
+                    case GHOST_START:
+                        ghostsStarts.add(new Point2D(i, j));
+                        break;
+                }
+            }
+        }
+        if (pacmanStartCounter != 1 || freeFieldsCounter == 0) {
+            throw new IllegalArgumentException("board has more than 1 Pac-Man start position or no free field");
+        }
+        this.board = board;
+        pacDotsOnStart = freeFieldsCounter;
     }
 
     public static Board parse(String[] boardDescription) {
@@ -38,6 +75,37 @@ public class Board {
             }
         }
         return new Board(tmpBoard);
+    }
+
+    /**
+     * Should be called when a new game or a new level starts. Initializes the fields, i.e. pac-dots, resets the
+     * power pellets (indicated by the original board), removes all actors and extra items.
+     */
+    public void initBoard() {
+        for (int i = 0; i < board.length; i++) {
+            for (int j = 0; j < board[0].length; j++) {
+                Field field = board[i][j];
+                switch (field.getType()) {
+                    case FREE:
+                        field.removeActor();
+                        field.setExtraItem(null);
+                        field.setPacDot(true);
+                        break;
+                    case POWER_PELLET:
+                        field.removeActor();
+                        field.setExtraItem(null);
+                        field.setPowerPellet(true);
+                        break;
+                    case PACMAN_START:
+                        field.removeActor();
+                        field.setExtraItem(null);
+                        break;
+                    case GHOST_START:
+                        field.removeActor();
+                        break;
+                }
+            }
+        }
     }
 
     public FieldInfo getFieldInfo(Point2D point) {
